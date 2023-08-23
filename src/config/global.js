8ac7a07859f8a0a9705c4cf068951240a9b4909f
Jhon Edinson Castañeda Oviedo <48686871+JhonEdinson-Shongo@users.noreply.github.com>
2023-08-22 23:38:36 -0500

    CFa

--- src/config/global.js
+++ src/config/global.js
@@ -84,7 +84,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/prueba.pdf',
+        download: 'downloads/331502_CF29_DU.pdf',
       },
       {
         icono: 'fas fa-download',
